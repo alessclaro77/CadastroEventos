9cfdada63b1ffbbe81ab0a8dc1aa8f72e21a07da
alessclaro <alessclaro@gmail.com>
2022-01-25 23:52:08 -0400

    sistema cadastro de eventos finalizado

---
+++ eventos.js
@@ -0,0 +1,29 @@
+//Data evento
+
+let dataEvent = 29/01/2022
+
+if(dataEvent > 25/01/2022) {
+    console.log("Data correta, pode cadastrar o Evento")
+  } else {
+    console.log("Data inválida, cadastro não permitido")
+  }
+
+ //Número de participantes
+ 
+ let numParticipantes = 120
+
+if(numParticipantes > 100) {
+    console.log("Cadastro não permitido, limite excedido!!")
+  } else {
+    console.log("Cadastro permitido!!!")
+  }
+
+  //Idade
+
+  let idade = 18
+
+if(idade < 18) {
+    console.log("Cadastro não permitido, Menor de idade!!")
+  } else {
+    console.log("Cadastro permitido!!!")
+  }
